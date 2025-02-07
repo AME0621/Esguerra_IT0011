d13cb64d7b2dc6c7967c07955221745f7d914532
AME0621 <amesguerra621@gmail.com>
2025-02-07 17:32:00 +0800

    Add files via upload

--- TSA1/ESGUERRA_TC23.py
+++ TSA1/ESGUERRA_TC23.py
@@ -1,1 +1,20 @@
-name = "Angelo"
+#TSA1 letter A
+n = 5 
+for i in range(1, 6):
+    print(" " * (n - i), end="") 
+    for j in range(1, i + 1):  
+        print(j, end="")  
+    print()  
+
+print() 
+
+#TSA1 letter B.
+for x in range (1, 6, 2):
+    for y in range (0, x):
+        print (x, end="")
+    print(" ")
+
+for x in range (6, 8):
+    for y in range (0, x):
+        print (x, end="")
+    print(" ")
